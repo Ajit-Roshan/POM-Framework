72e277828b5e8bc92e1726b9330ceb48980b2d29
rnc <rnc@rnc-Ideapad-Z570>
2023-05-29 22:53:37 +0530

    add new user module

---
+++ src/pages/AddUsers.java
@@ -0,0 +1,43 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Reporter;
+
+public class AddUsers {
+
+	WebDriver dr;
+
+	By nav= By.xpath("(//a[@class='oxd-main-menu-item'])[2]");
+	By addUser_Page= By.xpath("//button[normalize-space()='Add']");
+
+
+	By f1= By.xpath("//input[@placeholder='First Name']");	
+	By f2= By.xpath("//input[@placeholder='Middle Name']");
+	By f3= By.xpath("//input[@placeholder='Last Name']");
+
+	By saveBTN= By.xpath("//button[normalize-space()='Save']");	
+
+	By verifyUser= By.xpath("(//h6)[2]"); // contain text
+
+
+	public void addUser(String a, String b,String c) throws Throwable {
+		dr.findElement(nav).click();
+		dr.findElement(addUser_Page).click();
+
+		Thread.sleep(5000);
+
+		dr.findElement(f1).sendKeys(a);
+		dr.findElement(f2).sendKeys(b);
+		dr.findElement(f3).sendKeys(c);
+		dr.findElement(saveBTN).click();			
+
+		Thread.sleep(1000);
+	}
+
+
+	public AddUsers(WebDriver dr) {
+		// TODO Auto-generated constructor stub
+		this.dr= dr;
+	}
+}
--- src/test_Runner/Driver_Test.java
+++ src/test_Runner/Driver_Test.java
@@ -4,9 +4,11 @@ import java.io.FileInputStream;
 import java.util.Properties;
 
 import org.testng.Reporter;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import common.Setup_Teardown;
+import pages.AddUsers;
 import pages.LoginPage;
 import pages.LogoutPage;
 
@@ -28,7 +30,22 @@ public class Driver_Test extends Setup_Teardown{
 		Reporter.log("login status :: "+loginRes);		
 	}	
 	
-	@Test(priority = 2)
+	
+	@Test(priority = 2, dataProvider = "dataHolder")
+	public void addUser(String a1,String a2, String a3) throws Throwable {
+		AddUsers addUser= new AddUsers(dr);
+		addUser.addUser(a1, a2, a3);
+	}
+	
+	@DataProvider(name = "dataHolder")
+	public Object[][] dataCollections(){
+		return new  Object[][] {
+			{"aron" ,"brio" , "emuel"}
+		};
+	}
+	
+	
+	@Test(priority = 3)
 	public void logout() {
 		LogoutPage ougout= new LogoutPage(dr);
 		boolean outRes= ougout.verifyLogout();
